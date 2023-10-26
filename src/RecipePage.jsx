accd5975a597765dd2812038b5fe97440d1f7670
= <=>
2023-10-26 14:28:34 -0400

    last update

--- src/RecipePage.jsx
+++ src/RecipePage.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import "./RecipePage.css";
 import { supabase } from '../lib/supabaseClient';
 
 const RecipePage = () => {
-  const { id } = useParams(); // Extract id from URL parameters
+  const { id } = useParams(); 
   const [recipe, setRecipe] = useState(null);
 
   useEffect(() => {
@@ -29,17 +30,42 @@ const RecipePage = () => {
   }
 
   return (
-    <div>
-      <h1>{recipe.recipeName}</h1>
-      <p>{recipe.author}</p>
-      <p>{recipe.description}</p>
-      <p>{recipe.prepTime}</p>
-      <p>{recipe.cookTime}</p>
-      <p>{recipe.instructions}</p>
-      <img src={recipe.image} alt={recipe.recipeName} />
-      <p>{recipe.serving}</p>
+    <div className="recipe-page">
+      <div className="recipe-title">
+        <h1>{recipe.recipeName}</h1>
+      </div>
 
-      {/* Render other recipe details... */}
+      <div className="flex-container">
+        <div className="flex-item">
+            <img src={recipe.image} alt={recipe.recipeName} className="recipe-image" />
+        </div>
+
+        <div className="flex-item">
+           <h2 className='h2' style={{ marginBottom: '50px' }}>Description</h2>
+           <p ><strong>{recipe.description}</strong></p>
+        </div>
+     </div>
+
+     <div className="flex-container">
+        <div className="flex-item">
+          <h2 className='h2'>Created By:</h2>
+          <p><strong>{recipe.author}</strong></p>
+          <h2 className='h2'>Date:</h2>
+          <p><strong>{recipe.creation_date}</strong></p>
+        </div>
+
+        <div className="flex-item">
+          <p><h2 className='h2'>Serving:</h2><strong>{recipe.serving}</strong></p>
+          <p><h2 className='h2'>Preparation Time:</h2><strong>{recipe.prepTime}</strong></p>
+          <p><h2 className='h2'>Cooking Time:</h2><strong>{recipe.cookTime}</strong></p>
+        </div>
+
+        <div className="flex-item">
+         <h2 className='h2'>Instuction</h2>
+          <p><strong>{recipe.instructions}</strong></p>
+        </div>
+        
+      </div>
     </div>
   );
 };
